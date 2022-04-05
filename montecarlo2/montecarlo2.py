3396cb8eb63b165abeeb3bb454711f95988c1c81
FerentAnnaRenata <68391459+FerentAnnaRenata@users.noreply.github.com>
2022-04-05 11:08:18 +0300

    Update montecarlo2.py

--- montecarlo2/montecarlo2.py
+++ montecarlo2/montecarlo2.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def square_pi(n):
-    xy = np.random.random((n, 2)) ** 2
-    counts = np.sum((xy[:, 0] + xy[:, 1]) <= 1)
+def pi(n):
+    x_y = np.random.random((n, 2)) ** 2
+    counts = np.sum((x_y[:, 0] + x_y[:, 1]) <= 1)
     print("pi aproximat la:", 4*counts/n)
     return 4*counts/n
 
 
-square_pi(5000)
+pi(5000)
